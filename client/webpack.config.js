f052e08fdad46e0da27d004ab90881e050388b7c
Akafifi <itsmeafifi@gmail.com>
2023-10-19 22:12:30 -0500

    module and plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+
+      }),
+      new MiniCssExtractPlugin(),
+
+      new WebpackPwaManifest({
+        name: 'To The Text Editor',
+        short_name: 'Text Editor',
+        description: 'Edit Your Text Here',
+        display: 'standalone',
+        background_color: '#1e1e1e',
+        theme_color: '#1e1e1e',
+        start_url: '/',
+        publicPath: '/',
+        fingerprints: false,
+        inject: true,
+        icons: [
+                  {
+                    src: path.resolve('src/images/logo.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons'),
+                  },
+        ],
+
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], 
+        },
+        {
+          test: /.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+
+            }
+          }
+        }
         
       ],
     },
