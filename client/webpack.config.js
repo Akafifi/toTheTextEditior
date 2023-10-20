dbe28681b002755f00d0dd97057e71b13598dd96
Akafifi <itsmeafifi@gmail.com>
2023-10-19 23:21:41 -0500

    optimization

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -16,6 +17,12 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      publicPath: "",
+    },
+    optimization: {
+      minimier: [
+        new CssMinimizerPlugin()
+      ]
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -23,8 +30,6 @@ module.exports = () => {
         title: "Webpack Plugin",
 
       }),
-      new MiniCssExtractPlugin(),
-
       new WebpackPwaManifest({
         name: 'To The Text Editor',
         short_name: 'Text Editor',
@@ -45,8 +50,7 @@ module.exports = () => {
         ],
 
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
+      new GenerateSW({
         swDest: 'src-sw.js'
       })
     ],
@@ -55,7 +59,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'], 
+          use: [MiniCssExtractPlugin.loader, 'css-loader'], 
         },
         {
           test: /.m?js$/,
